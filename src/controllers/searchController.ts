0318259b1523f29ffcdf7aeadc8a55cf59c60358
MatokoLB <matheuslisboabr@gmail.com>
2023-02-11 00:18:04 -0300

    add base do projeto

---
+++ src/controllers/searchController.ts
@@ -0,0 +1,5 @@
+import { Request ,Response } from "express";
+
+export const search = (req: Request ,res: Response ) =>{
+    //ree.render("pages/search")
+}
---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import dotenv from "dotenv"
+import Express  from "express";
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRouter from "./routes/index"
+
+dotenv.config()
+
+const server = Express();
+
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname,"views"))
+server.engine('mustache', mustache())
+
+
+server.use(Express.static(path.join(__dirname,'..public')))
+
+//roteas
+server.use(mainRouter)
+
+server.use((req, res)=>{
+    res.send("pagina nao encontradoa!")
+} )
+
+
+server.listen(process.env.PORT)
